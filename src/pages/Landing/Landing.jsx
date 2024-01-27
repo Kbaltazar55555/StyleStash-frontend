8c3f4be08c042ee471b8fbd13e1e31f14a4d94d5
Antwon Frager Jr <Antwon.frager@gmail.com>
2024-01-27 17:57:11 -0500

    video redering

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -8,12 +8,15 @@ const Landing = ({ handleSignupOrLogin }) => {
   const [message, setMessage] = useState('');
   const [currentForm, setcurrentForm] = useState('login'); // Tracks the current form
 
-  const toggleForm = () => {
-    setcurrentForm(currentForm === 'login' ? 'signup' : 'login');
-  }
-
   return (
     <div className="landing-container">
+      <div className="brand-container">
+        <h1 className="brand-title">Style$tash</h1>
+        <p className="brand-subtitle">Your wardrobe on the Go</p>
+        <p className="brand-description">
+          Welcome to StyleStash, the innovative app created by Array of Attire that revolutionizes your shopping and wardrobe management experience! Have you ever found yourself in a store, wondering if you already own something similar to what's in your hand? StyleStash is your modern solution to this age-old dilemma.
+        </p>
+      </div>
       {currentForm === 'login' ? (
         <LoginForm 
           message={message} 
@@ -27,7 +30,7 @@ const Landing = ({ handleSignupOrLogin }) => {
           handleSignupOrLogin={handleSignupOrLogin}
         />
       )}
-      <button onClick={toggleForm} className="toggle-button">
+      <button onClick={() => setcurrentForm(currentForm === 'login' ? 'signup' : 'login')} className="toggle-button">
         {currentForm === 'login' ? 'Sign Up' : 'Log In'}
       </button>
     </div>
